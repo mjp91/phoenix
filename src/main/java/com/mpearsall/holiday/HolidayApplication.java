5af496577556d0dbee9fd647886df8d3151c349a
Matthew Pearsall <mjp91@live.co.uk>
2019-05-11 17:23:16 +0100

    Initial Commit

---
+++ src/main/java/com/mpearsall/holiday/HolidayApplication.java
@@ -0,0 +1,13 @@
+package com.mpearsall.holiday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HolidayApplication.class, args);
+  }
+
+}
