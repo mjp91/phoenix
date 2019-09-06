b65e8c725d0d4231ab67780281ac5d2267d911b7
Matthew Pearsall <mjp91@live.co.uk>
2019-09-06 23:00:06 +0100

    Secured user controller; Spring Boot 2.1.8

--- src/main/java/com/mpearsall/hr/controller/UserController.java
+++ src/main/java/com/mpearsall/hr/controller/UserController.java
@@ -3,12 +3,14 @@ package com.mpearsall.hr.controller;
 import com.mpearsall.hr.entity.User;
 import com.mpearsall.hr.repository.UserRepository;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@Secured("ROLE_ADMIN")
 public class UserController {
   private final UserRepository userRepository;
 
