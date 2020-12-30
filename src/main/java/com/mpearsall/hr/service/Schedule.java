3db0cb6987208d4c3022319595e4be94e1c38747
Matthew Pearsall <mjp91@live.co.uk>
2020-12-30 23:08:39 +0000

    Removed license stuff and updated SendGrid API key

--- src/main/java/com/mpearsall/hr/service/Schedule.java
+++ src/main/java/com/mpearsall/hr/service/Schedule.java
@@ -1,21 +1,9 @@
 package com.mpearsall.hr.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class Schedule {
-  private final LicenseService licenseService;
-
-  public Schedule(LicenseService licenseService) {
-    this.licenseService = licenseService;
-  }
-
-  @Scheduled(fixedDelayString = "PT1H")
-  void updateLicense() {
-    log.info("Updating license");
-    licenseService.updateLicense();
-  }
 }
